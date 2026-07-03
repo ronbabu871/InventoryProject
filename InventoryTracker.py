a8a0db621e37657ef941aa9f776f37423af4f67a
Ron Babu <ronbabu871@gmail.com>
2026-07-03 11:19:52 -0400

    Project setup by Ron Babu

--- InventoryTracker.py
+++ InventoryTracker.py
@@ -4,11 +4,3 @@ class InventoryTracker:
 
     def addItem(self, itemName, quantity):
         self.inventory[itemName] = quantity
-
-    def checkStockLevel(self, itemName):
-        return self.inventory.get(itemName, 0)
-
-    def alertLowStock(self, itemName):
-        if self.checkStockLevel(itemName) < 5:
-            return f"Warning: {itemName} is running low."
-        return f"{itemName} has enough stock."
